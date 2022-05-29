8cc1af72266d516e135ca41011cef5ad257e4335
Mauro Erben <mauroerben100@hotmail.com>
2022-05-29 12:23:06 -0300

    changes in technologies

--- src/components/main/tecnologias/items.jsx
+++ src/components/main/tecnologias/items.jsx
@@ -1,15 +1,11 @@
-import { Icon, VStack, Text } from "@chakra-ui/react"
+import { Icon, Text } from "@chakra-ui/react"
 import TechnologyBox from "../../box/technologyBox"
 
 const Item = ({ icon, color, name }) => {
     return (
         <TechnologyBox>
-            <VStack
-                spacing={[2, 4]}
-            >
-                <Icon fontSize={60} color={color} as={icon} />
-                <Text fontSize='md'>{name}</Text>
-            </VStack>
+            <Icon fontSize={[40, 60]} color={color} as={icon} />
+            <Text fontSize='md'>{name}</Text>
         </TechnologyBox>
     )
 }
--- src/components/main/tecnologias/tecnologias.jsx
+++ src/components/main/tecnologias/tecnologias.jsx
@@ -1,11 +1,11 @@
-import { Stack, Text } from '@chakra-ui/react'
+import { Box, Text, SimpleGrid } from '@chakra-ui/react'
 import Item from './items'
 import {
     SiReact,
     SiRedux,
-    SiChakraui
+    SiChakraui,
+    SiMysql,
 } from 'react-icons/si'
-import { FaDatabase } from 'react-icons/fa'
 
 import AnimatedBox from '../../box/animatedBox'
 
@@ -13,15 +13,16 @@ const Tecnologias = () => {
     return (
         <AnimatedBox>
             <Text id="tecnologias" fontWeight='bold'>Tecnologías</Text>
-                <Stack
-                    spacing={[6, 8, 10]}
-                    padding={[2, 4, 6, 8]}
-                    direction='row'
-                >
+            <Box
+                padding={[4, 4, 6, 8]}
+            >
+                <SimpleGrid columns={[2, 4]} spacing={10}>
                     <Item icon={SiReact} color='#5ed9fb' name='React' />
                     <Item icon={SiRedux} color='#793eb8' name='Redux' />
-                    <Item icon={FaDatabase} color='#656565' name='SQL' />
-                </Stack>
+                    <Item icon={SiChakraui} color='#60caca' name='Chakra UI' />
+                    <Item icon={SiMysql} color='#5d9ab4' name='MySQL' />
+                </SimpleGrid>
+            </Box>
         </AnimatedBox>
     )
 }
